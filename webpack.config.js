73640eb6b3da7ef01d015397a33a5091f461280c
aayushigupt <aayushigupta108@gmail.com>
2025-06-28 12:26:00 +0530

    Episode - 4 JSX, Babel and Start of Food App

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 // webpack.config.js
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, "dist"),
     },
     compress: true,
     port: 3000,
@@ -24,20 +24,24 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html', // Use your existing index.html as a template
+      template: "./dist/index.html", // Use your existing index.html as a template
     }),
   ],
   cache: {
-    type: 'filesystem', // Enables persistent caching
-    cacheDirectory: path.resolve(__dirname, '.custom-cache'), // Custom cache directory
+    type: "filesystem", // Enables persistent caching
+    cacheDirectory: path.resolve(__dirname, ".custom-cache"), // Custom cache directory
   },
 };
